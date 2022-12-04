ac5d980821aafa58f407d4c806b86d4fbd1ac24b
Mark van Venrooij <mark@mvvenrooij.nl>
2022-12-04 08:10:35 +0100

    Larger than amount rule created

---
+++ src/main/java/nl/mvvenrooij/financial/categorization/LargerThanAmountRule.java
@@ -0,0 +1,24 @@
+package nl.mvvenrooij.financial.categorization;
+
+import nl.mvvenrooij.financial.domain.Category;
+import nl.mvvenrooij.financial.domain.Transaction;
+import org.javamoney.moneta.Money;
+
+public class LargerThanAmountRule implements CategoryRule {
+    private final Category category;
+    private final Money amountToCompareTo;
+
+    public LargerThanAmountRule(final Category category, final Money amountToCompareTo) {
+        this.category = category;
+        this.amountToCompareTo = amountToCompareTo;
+    }
+
+    @Override
+    public boolean categorize(final Transaction transaction) {
+        if (amountToCompareTo.isLessThan(transaction.amount())) {
+            category.addTransactions(transaction);
+            return true;
+        }
+        return false;
+    }
+}
