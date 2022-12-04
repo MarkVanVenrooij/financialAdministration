75815c7f3d6f1f307e467af1e48b36105d6ce67e
Mark van Venrooij <mark@mvvenrooij.nl>
2022-12-04 08:25:40 +0100

    refactored amount rule to prevent duplication

--- src/main/java/nl/mvvenrooij/financial/categorization/SmallerThanAmountRule.java
+++ src/main/java/nl/mvvenrooij/financial/categorization/SmallerThanAmountRule.java
@@ -1,25 +1,11 @@
 package nl.mvvenrooij.financial.categorization;
 
 import nl.mvvenrooij.financial.domain.Category;
-import nl.mvvenrooij.financial.domain.Transaction;
 import org.javamoney.moneta.Money;
 
-public class SmallerThanAmountRule implements CategoryRule{
-    private final Category category;
-    private final Money amountToCompareTo;
+public class SmallerThanAmountRule extends AmountRule {
 
     public SmallerThanAmountRule(final Category category, final Money amountToCompareTo) {
-
-        this.category = category;
-        this.amountToCompareTo = amountToCompareTo;
-    }
-
-    @Override
-    public boolean categorize(final Transaction transaction) {
-        if (amountToCompareTo.isGreaterThan(transaction.amount())) {
-            category.addTransactions(transaction);
-            return true;
-        }
-        return false;
+        super(category, amountToCompareTo::isGreaterThan);
     }
 }
