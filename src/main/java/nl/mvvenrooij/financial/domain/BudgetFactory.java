ae0fa9f5e11dfdd807e8e980f5809c4d18506ff6
Mark van Venrooij <mark@mvvenrooij.nl>
2023-05-15 10:05:39 +0200

    Make sure that rounding works correctly when evenly spreading the budget across the months

--- src/main/java/nl/mvvenrooij/financial/domain/BudgetFactory.java
+++ src/main/java/nl/mvvenrooij/financial/domain/BudgetFactory.java
@@ -2,6 +2,11 @@ package nl.mvvenrooij.financial.domain;
 
 import org.javamoney.moneta.Money;
 
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import javax.money.MonetaryRounding;
+import javax.money.RoundingQueryBuilder;
+import java.math.RoundingMode;
 import java.time.Month;
 import java.time.Year;
 import java.util.Arrays;
@@ -10,6 +15,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BudgetFactory {
+    public static final CurrencyUnit EUR = Monetary.getCurrency("EUR");
     private final CategoryRepository categoryRepository;
 
     public BudgetFactory(final CategoryRepository categoryRepository) {
@@ -20,10 +26,15 @@ public class BudgetFactory {
     public Budget createEvenlySpreadBudget(final String categoryName, final Year year, final Money amount) {
         if (categoryRepository.findCategoryByName(categoryName).isPresent()) {
 
-            Map<Month, Money> amountPlanned = Arrays.stream(Month.values())
+            final Map<Month, Money> amountPlanned = Arrays.stream(Month.values())
                     .map(month -> Collections.singletonMap(month, spreadAmountAmongstTwelfeMonths(amount)))
                     .flatMap(map -> map.entrySet().stream())
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+            final Money currentAmountPlanned = amountPlanned.values().stream().reduce(Money.zero(EUR), Money::add);
+            final Money remainderAmount = amount.subtract(currentAmountPlanned);
+
+            amountPlanned.put(Month.DECEMBER, amountPlanned.get(Month.DECEMBER).add(remainderAmount));
 
 
             return new Budget(categoryName, year, amountPlanned);
@@ -44,6 +55,8 @@ public class BudgetFactory {
     }
 
     private Money spreadAmountAmongstTwelfeMonths(Money amount) {
-        return amount.divide(12);
+        MonetaryRounding rounding = Monetary.getRounding(
+                RoundingQueryBuilder.of().setScale(2).set(RoundingMode.HALF_DOWN).build());
+        return amount.divide(12).with(rounding);
     }
 }
